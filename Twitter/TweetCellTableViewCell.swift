514c58794dcfd456de58045d245d733be060f343
Rudransh Singh <rudransh99singh@gmail.com>
2021-03-14 11:52:22 -0700

    Twitter Part 2

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -103,12 +103,6 @@ class TweetCellTableViewCell: UITableViewCell {
         }
     }
     
-    
-    
-    
-    
-    
-    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
